21508619da729728cb086399627d71b06ac4b70f
wufuqiang <wufuqiang@buaa.edu.cn>
2020-12-29 13:08:11 +0800

    ADD:NIO客户端

---
+++ src/main/java/com/wufuqiang/nio/NIOClient.java
@@ -0,0 +1,36 @@
+package com.wufuqiang.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class NIOClient {
+    public static void main(String[] args) throws IOException {
+        //得到一个网络通道
+        SocketChannel socketChannel = SocketChannel.open();
+        //设置为非阻塞方式
+        socketChannel.configureBlocking(false);
+
+        InetSocketAddress inetSocketAddress = new InetSocketAddress("127.0.0.1", 6666);
+        //连接服务器
+        if(!socketChannel.connect(inetSocketAddress)){
+            while (!socketChannel.finishConnect()){
+                System.out.println("因为连接需要时间，客户端不会阻塞，可以做其它工作...");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //连接服务器成功
+        String str = "吴福强";
+
+        ByteBuffer buffer = ByteBuffer.wrap(str.getBytes());
+
+        socketChannel.write(buffer);
+        System.in.read();
+    }
+}
