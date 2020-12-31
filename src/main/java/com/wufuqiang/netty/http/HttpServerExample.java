87b033fd2289d7441c15ad00a7a6eebab4213969
wufuqiang <wufuqiang@buaa.edu.cn>
2020-12-31 18:15:16 +0800

    ADD:netty实现http server端的example

---
+++ src/main/java/com/wufuqiang/netty/http/HttpServerExample.java
@@ -0,0 +1,41 @@
+package com.wufuqiang.netty.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class HttpServerExample {
+    public static void main(String[] args) throws Exception {
+        //1 创建bossGroup的Nio事件循环组
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        //2 创建workerGroup的Nio事件循环组
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            //3 创建服务端引导程序
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            //4 为引导程序指定事件组，包括bossGroup和workerGroup
+            serverBootstrap.group(bossGroup, workerGroup)
+                    //5 指定服务器通道
+                    .channel(NioServerSocketChannel.class)
+                    //6 设置一些配置参数
+                    //7 指定handler
+                    .childHandler(new HttpServerInitializerExample());
+
+            //8 绑定端口
+            ChannelFuture channelFuture = serverBootstrap.bind(6668).sync();
+            //9 添加监听器
+//            channelFuture.addListener()
+            System.out.println("wufuqiang");
+            //10 关闭端口
+            channelFuture.channel().closeFuture().sync();
+            System.out.println("wufuqiang");
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
