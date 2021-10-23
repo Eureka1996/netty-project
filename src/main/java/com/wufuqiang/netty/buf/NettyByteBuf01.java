39071d0ea65a967c7efeab0ff1409554418b898f
wufuqiang <wufuqiang@qq.com>
2021-10-23 21:13:54 +0800

    MOD:ByteBuf

--- src/main/java/com/wufuqiang/netty/buf/NettyByteBuf01.java
+++ src/main/java/com/wufuqiang/netty/buf/NettyByteBuf01.java
@@ -1,15 +1,19 @@
 package com.wufuqiang.netty.buf;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.Unpooled;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 
+@Slf4j
 public class NettyByteBuf01 {
     public static void main(String[] args) {
-
-
+        
         //创建一个ByteBuf
         //说明
-        //1. 创建 对象，该对象包含一个数组arr , 是一个byte[10]
+        //1. 创建对象，该对象包含一个数组arr , 是一个byte[10]
         //2. 在netty 的buffer中，不需要使用flip 进行反转
         //   底层维护了 readerindex 和 writerIndex
         //3. 通过 readerindex 和  writerIndex 和  capacity， 将buffer分成三个区域
@@ -17,7 +21,7 @@ public class NettyByteBuf01 {
         // readerindex---writerIndex ， 可读的区域
         // writerIndex -- capacity, 可写的区域
         ByteBuf buffer = Unpooled.buffer(10);
-
+        log.debug("ByteBuffer:"+buffer);
         for(int i = 0; i < 10; i++) {
             buffer.writeByte(i);
         }
@@ -31,5 +35,36 @@ public class NettyByteBuf01 {
             System.out.println(buffer.readByte());
         }
         System.out.println("执行完毕");
+    }
+    
+    @Test
+    public void byteBufferPool(){
+        final ByteBuf buf1 = ByteBufAllocator.DEFAULT.buffer();
+        log.debug("buf1--->{}",buf1);
+
+        final ByteBuf buf2 = ByteBufAllocator.DEFAULT.heapBuffer();
+        log.debug("buf2--->{}",buf2);
+
+        final ByteBuf buf3 = Unpooled.buffer();
+        log.debug("buf3--->{}",buf3);
+    }
+    
+    @Test
+    public void byteBufSlice(){
+        final ByteBuf buf = ByteBufAllocator.DEFAULT.buffer(10);
+        buf.writeBytes(new byte[]{'a','b','c','d','e','f','g','h','i','j'});
+
+        final ByteBuf slice1 = buf.slice(0, 5);
+        final ByteBuf slice2 = buf.slice(5, 5);
+
+        slice1.setByte(0,'P');
+        
+        log.debug("buf={}",buf.toString(CharsetUtil.UTF_8));
+        final String slice1Str = slice1.toString(CharsetUtil.UTF_8);
+        log.debug("slice1Str={}",slice1Str);
+
+        log.debug("slice2Str={}",slice2.toString(CharsetUtil.UTF_8));
+
+
     }
 }
